f678071248f4584623428f60fa2ea875f7e84e5b
Günther Rezniczek <g.rezniczek@gmail.com>
2022-03-23 14:58:45 +0100

    Add test switcher

--- DummyExternalModule.php
+++ DummyExternalModule.php
@@ -16,14 +16,25 @@ class DummyExternalModule extends AbstractExternalModule {
 
         // $js_url_api = $this->getApiEndpointUrl("js/test_api.js");
         // print "<script type=\"text/javascript\" src=\"{$js_url_api}\"></script>";
-        
-        $this->initializeJavascriptModuleObject();
-        $jsmo_name = $this->getJavascriptModuleObjectName();
 
-        $js_url_langswitch = $this->getUrl("js/test_langswitch.js");
-        print "<script type=\"text/javascript\" src=\"{$js_url_langswitch}\"></script>";
-        print "<script>window.DE_RUB_DummyExternalModule.init({$jsmo_name});</script>";
+        if ($this->getSystemSetting("test-afterrender") == 1) {
+            $this->initializeJavascriptModuleObject();
+            $jsmo_name = $this->getJavascriptModuleObjectName();
+    
+            $js_url_langswitch = $this->getUrl("js/test_langswitch.js");
+            print "<script type=\"text/javascript\" src=\"{$js_url_langswitch}\"></script>";
+            print "<script>window.DE_RUB_DummyExternalModule.init({$jsmo_name});</script>";
+        }
 
+        if ($this->getSystemSetting("test-setsystemsetting") == 1) {
+            $orig = $this->getSystemSetting("test");
+            $this->setSystemSetting("test", $project_id == null ? "System value" : "Project {$project_id} value");
+            $new = $this->getSystemSetting("test");
+            ?>
+            <script>
+                alert('Set System Value Test: Original = "<?=json_encode($orig)?>", New = "<?=json_encode($new)?>".');
+            </script>
+            <?php
+        }
     }
-
 }
